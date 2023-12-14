2196888d628050bfe233df78c5762f40f561cf32
loren <lorenmichaelh@gmail.com>
2023-12-14 15:15:11 -0600

    change prompts to few shots

--- index.js
+++ index.js
@@ -15,14 +15,27 @@ document.getElementById("send-btn").addEventListener("click", () => {
     setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`;
     movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`;
     fetchBotReply(userInput);
-    fetchSynopsis(userInput);
+    // fetchSynopsis(userInput);
   }
 });
 
 async function fetchBotReply(outline) {
   const response = await openai.completions.create({
     model: 'text-davinci-003',
-    prompt: `Generate a short message to say "${outline}" sounds interesting and that you need some minutes to think about it. Mention one aspect of the sentence.`,
+    prompt: `Generate a short message to enthusiastically say an outline sounds interesting and that you need some minutes to think about it.
+    ###
+    outline: Two dogs fall in love and move to Hawaii to learn to surf.
+    message: I'll need to think about that. But your idea is amazing! I love the bit about Hawaii!
+    ###
+    outline: A plane crashes in the jungle and the passengers have to walk 1000km to safety.
+    message: I'll spend a few moments considering that. But I love your idea!! A disaster movie in the jungle!
+    ###
+    outline: A group of corrupt lawyers try to send an innocent woman to jail.
+    message: Wow that is awesome! Corrupt lawyers, huh? Give me a few moments to think!
+    ###
+    outline: ${outline}
+    message: 
+    `,
     max_tokens: 60
   });
   movieBossText.innerText = response.data.choices[0].text.trim();
@@ -31,7 +44,14 @@ async function fetchBotReply(outline) {
 async function fetchSynopsis(outline) {
   const response = await openai.completions.create({
     model: 'text-davinci-003',
-    prompt: `Generate an engaging, professional and marketable movie synopsis based on the following idea: ${outline}`,
+    prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline
+    ###
+    outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
+    synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
+    ###
+    outline: ${outline}
+    synopsis:
+    `,
     max_tokens: 700
   })
   document.getElementById('output-text').innerText = response.data.choices[0].text.trim();
