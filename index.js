b5bc44b12169ea6dc1eea618f47c9b5dff355014
loren <lorenmichaelh@gmail.com>
2023-12-14 13:28:42 -0600

    working through imports, starting dependency

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import { process } from '/env';
+
+
 const setupTextarea = document.getElementById('setup-textarea') ;
 const setupInputContainer = document.getElementById('setup-input-container');
 const movieBossText = document.getElementById('movie-boss-text');
@@ -7,20 +10,24 @@ document.getElementById("send-btn").addEventListener("click", () => {
     setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
     movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
   // }
+  fetchBotReply();
 });
 
 const url = "https://api.openai.com/v1/completions"
+const apiKey = process.env.OPENAI_API_KEY
 
-fetch(url, {
-  method: "POST",
-  headers: {
-    "content-type": "Application/json",
-    "Authorization": `Bearer ${APIKEY}`
-  },
-  body: JSON.stringify({
-    'model': 'text-davinci-003',
-    'prompt': 'Sound enthusiastic in five words or less'
-  })
-}).then(r => r.json().then(data => 
-  movieBossText.innerText = data.choices[0].text
-))
+function fetchBotReply() {
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "content-type": "Application/json",
+      "Authorization": `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      'model': 'text-davinci-003',
+      'prompt': 'Sound enthusiastic in five words or less'
+    })
+  }).then(r => r.json().then(data => 
+    movieBossText.innerText = data.choices[0].text
+  ))
+}
