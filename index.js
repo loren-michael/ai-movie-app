7903952177a01827c23d7af8c63aaef5c66d2eb5
loren <lorenmichaelh@gmail.com>
2023-12-14 14:44:58 -0600

    adding synopsis fetch, still fixing import bug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { process } from '/env';
-import OpenAI from 'openai';
+import OpenAI, { Configuration } from './node_modules/openai';
 // import { Configuration, OPENAIApi } from 'openai';
 
 
@@ -20,14 +20,24 @@ document.getElementById("send-btn").addEventListener("click", () => {
     setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`;
     movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`;
     fetchBotReply(userInput);
+    fetchSynopsis(userInput);
   }
 });
 
 async function fetchBotReply(outline) {
-  const response = await openai.chat.completions.create({
+  const response = await openai.completions.create({
     model: 'text-davinci-003',
     prompt: `Generate a short message to say "${outline}" sounds interesting and that you need some minutes to think about it. Mention one aspect of the sentence.`,
     max_tokens: 60
   });
   movieBossText.innerText = response.data.choices[0].text.trim();
+}
+
+async function fetchSynopsis(outline) {
+  const response = await openai.completions.create({
+    model: 'text-davinci-003',
+    prompt: `Generate an engaging, professional and marketable movie synopsis based on the following idea: ${outline}`,
+    max_tokens: 700
+  })
+  document.getElementById('output-text').innerText = response.data.choices[0].text.trim();
 }
