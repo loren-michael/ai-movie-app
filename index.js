d4332fcc648fa8b4d953c73b361bf92aa2bcf7a2
loren <lorenmichaelh@gmail.com>
2023-12-14 14:16:40 -0600

    remove second openai import to fix error, refactor button functionality and primpt

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { process } from '/env';
-import openai from 'openai';
+// import openai from 'openai';
 import { Configuration, OPENAIApi } from 'openai';
 
 
@@ -14,17 +14,19 @@ const setupInputContainer = document.getElementById('setup-input-container');
 const movieBossText = document.getElementById('movie-boss-text');
 
 document.getElementById("send-btn").addEventListener("click", () => {
-  // if (setupTextarea.value) {
-    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
-    movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
-  // }
-  fetchBotReply();
+  if (setupTextarea.value) {
+    const userInput = setupTextarea.value
+    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`;
+    movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`;
+    fetchBotReply(userInput);
+  }
 });
 
-async function fetchBotReply() {
+async function fetchBotReply(outline) {
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: 'Sound enthusiastic in five words or less'
+    prompt: `Generate a short message to say "${outline}" sounds interesting and that you need some minutes to think about it. Mention one aspect of the sentence.`,
+    max_tokens: 60
   });
   movieBossText.innerText = response.data.choices[0].text.trim();
 }
